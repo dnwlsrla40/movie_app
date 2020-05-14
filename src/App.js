853f8cf38a6fc4648f6a7a4ab8cef81d684c5ed3
dnwlsrla40 <dnwlsrla40@naver.com>
2020-05-14 18:14:50 +0900

    #3.1 All you need to know about State - Class Component, setState, etc...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types'; // Prop가 전송하는 데이터 check
 
+/*
+props 에서 사용
 const foodILike = [
   {
     id: 1,
@@ -66,6 +68,28 @@ function App() {
       ))}
     </div>
   );
+}
+*/
+
+class App extends React.Component{
+  state = {
+    count : 0
+  };
+  add = () => {
+    console.log("add");
+    this.setState(current => ({count: current.count+1}));
+  };
+  minus = () => {
+    console.log("minus");
+    this.setState(current => ({count: current.count-1}));
+  }
+  render(){
+    return <div>
+    <h1>The number is : {this.state.count}</h1>
+    <button onClick = {this.add}>Add</button>
+    <button onClick = {this.minus}>Minus</button>
+    </div>;  
+  }
 }
 
 export default App;
