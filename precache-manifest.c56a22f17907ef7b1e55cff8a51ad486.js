847f1c9da47a04b49c123738b36de8abe2ca3408
dnwlsrla40 <dnwlsrla40@naver.com>
2020-05-24 20:22:21 +0900

    Updates

---
+++ precache-manifest.c56a22f17907ef7b1e55cff8a51ad486.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a489de91fa3d4a378d0fdffa94b25655",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "f5976ff324bc20a72c6a",
+    "url": "/movie_app/static/css/main.1b6d7bf6.chunk.css"
+  },
+  {
+    "revision": "964546851f2431c5d268",
+    "url": "/movie_app/static/js/2.2d3cfcc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.2d3cfcc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5976ff324bc20a72c6a",
+    "url": "/movie_app/static/js/main.011b8fda.chunk.js"
+  },
+  {
+    "revision": "ffbd4de4896f1a599175",
+    "url": "/movie_app/static/js/runtime-main.0186e20d.js"
+  }
+]);
